e606d6027ba1a496d56ec75af47463b4655656f4
Celal Zamanoglu <celal.zamanoglu@hotmail.com>
2025-06-10 02:53:31 +0300

    fix: build canceled bc of uncommented pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { HeroSection } from "@/components/HeroSection";
-import { AboutSection } from "@/components/AboutSection";
+/* import { AboutSection } from "@/components/AboutSection";
 import { ServicesSection } from "@/components/ServicesSection";
 import { GallerySection } from "@/components/GallerySection";
 import { TestimonialsSection } from "@/components/TestimonialsSection";
-import { ContactSection } from "@/components/ContactSection";
+import { ContactSection } from "@/components/ContactSection"; */
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
 
